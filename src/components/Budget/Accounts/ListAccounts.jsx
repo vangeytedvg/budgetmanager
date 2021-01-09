65b26bcf98d6af1d9a4425d637579f3bb77c43f5
vangeytedvg@gmail.com <vangeytedvg@gmail.com>
2021-01-09 13:32:44 +0100

    first commit

--- src/components/Budget/Accounts/ListAccounts.jsx
+++ src/components/Budget/Accounts/ListAccounts.jsx
@@ -9,7 +9,7 @@ import InputMask from "react-input-mask";
 import TextError from "../../UI_Utils/TextError";
 
 const ListAccounts = (props) => {
-  // Array to hold the list of todos from firebase
+  // Accounts array for firebase
   const [accounts, setAccounts] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [accountOwner, setAccountOwner] = useState("");
@@ -17,14 +17,52 @@ const ListAccounts = (props) => {
   const [bank, setBank] = useState("");
   const [balance, setBalance] = useState(0);
 
+  // Get the current user
   const { currentUser, logout } = useAuth();
 
   /**
-   * Delete a todo based on it's id
-   * @param {*} id
+   * Styling
    */
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      "& .MultiFormControl-root": {
+        width: "80%",
+        margin: theme.spacing(1),
+      },
+    },
+    accountOwnerName: {
+      minWidth: "300px",
+    },
+    toolbar: theme.mixins.toolbar,
+    content: {
+      flexGrow: 1,
+      backgroundColor: theme.palette.background.default,
+      padding: theme.spacing(3),
+    },
+    crumb: {
+      color: "#1d5f43",
+    },
+    pageContent: {
+      marginTop: "15px",
+      maxWidth: "100%",
+      padding: theme.spacing(3),
+      display: "flex",
+      justifyContent: "center",
+      alignContent: "center",
+      alignItems: "center",
+    },
+    blabla: {
+      padding: "18px",
+      borderRadius: "5px",
+      backgroundColor: "#535353",
+      borderBlockStyle: "none",
+      color: "white",
+      fontFamily: "Roboto",
+    },
+  }));
+
   const deleteAccount = () => {
-    setShowMessageToDelete(false)
+    setShowMessageToDelete(false);
     // const id = e.id
     db.collection("accounts")
       .doc(idToDelete)
@@ -44,30 +82,6 @@ const ListAccounts = (props) => {
         });
       });
     setShowMessage(false);
-  };
-
-  /**
-   * Update the record given by id
-   * @param {U} id
-   */
-  const editItem = (id, state) => {
-    db.collection("accounts")
-      .doc(id)
-      .update({ done: !state })
-      .then(() => {
-        return toast("Todo updated", {
-          position: toast.POSITION.BOTTOM_RIGHT,
-          type: "info",
-          autoClose: 1000,
-        });
-      })
-      .catch((err) => {
-        return toast("Error", {
-          position: toast.POSITION.BOTTOM_RIGHT,
-          type: "warning",
-          autoClose: 1000,
-        });
-      });
   };
 
   /**
@@ -91,79 +105,27 @@ const ListAccounts = (props) => {
    * useEffect hook
    */
   useEffect(() => {
-    // On page load get the todos
+    // On page load get the accounts
     getAccounts();
   }, []);
 
   return (
-    <>
-      <MessageBox
-        show={showMessageToDelete}
-        message="Are you sure to delete the selected account?"
-        handleCancel={handleCancelDelete}
-        handleYes={deleteAccount}
-      />
-      <div className="todo-app">
-        <div className="spinner">
-          {/* Show progress indicator while loading data from Firestore */}
-          {isLoading && <Loader type="ThreeDots" color="#2500ee" />}
-        </div>
-        <div className="btn-add-container">
-          <span className="todo-title-header">Available accounts</span>
-          <Button className="btn btn-primary" onClick={handleShowModal}>
-            New Account
-            <FontAwesomeIcon
-              icon={faPlusSquare}
-              className="ml-2"
-            ></FontAwesomeIcon>
-          </Button>
-        </div>
-        <Table responsive striped hover variant="dark">
-          <thead>
-            <tr>
-              <th>Account Owner</th>
-              <th>Account Nr</th>
-              <th>Bank</th>
-              <th>Created</th>
-              <th>Balance</th>
-            </tr>
-          </thead>
-          <tbody>
-            {/* Display a message when there are no todos */}
-            {accounts.length === 0 && (
-              <tr key={0}>
-                <td colSpan="5">No Accounts</td>
-              </tr>
-            )}
-            {/* Display the todos if there are any */}
-            {accounts.map((account, key) => {
-              return (
-                <tr key={key}>
-                  <td>{account.owner}</td>
-                  <td>{account.accountnr}</td>
-                  <td>{account.bank}</td>
-                  <td>{account.date_created}</td>
-                  <td>€{account.balance}</td>
-                  <Button
-                    onClick={() => onAskToDelete(account.id)}
-                    variant="primary"
-                  >
-                    <FontAwesomeIcon icon={faEraser}></FontAwesomeIcon>
-                  </Button>
-                </tr>
-              );
-            })}
-          </tbody>
-        </Table>
-      </div>
-      <AccountsModal
-        showModal={showModal}
-        setShowModal={setShowModal}
-        handleCancelModal={handleCancelModal}
-        handleCloseModal={handleCloseModal}
-        existingData = {null}
-      />
-    </>
+    <div className={classes.content}>
+      <div className={classes.toolbar} />
+      <SectionTitle maintitle="Dashboard" subtitle="algemeen overzicht" />
+      <Grid
+        container
+        spacing={4}
+        className={`${classes.gridContainer} ${classes.gridMarginTop}`}
+      >
+        <Grid item xs={12} md={6} lg={4}>
+          <Card className={classes.root} raised>
+            <CardHeader title="Rekeningen" subheader="huidige stand" />
+            <CardContent></CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </div>
   );
 };
 
